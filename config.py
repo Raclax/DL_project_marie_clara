77ce7ba5c376bf3c10eb3b5507bec6a2d6e8b71d
Marie! <marie-point@hotmail.fr>
2025-12-07 17:26:51 +0100

    config file

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-root = r"C:\Users\Marie\Documents\info m2 local\DLCV\projet\CROHME2019_train_png"
-root_micro = r"C:\Users\Marie\Documents\info m2 local\DLCV\projet\CROHME2019_micro_train_tmp"
+root = r"C:\Users\Marie\Documents\info m2 local\Deep Learning CV\data\FullExpressions\CROHME2019_train_png"
+root_micro = r"C:\Users\Marie\Documents\info m2 local\Deep Learning CV\data\FullExpressions\CROHME2019_train_png"
